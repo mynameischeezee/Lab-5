99176d4d2c4bcbfc89dbceeb67d5ac898881e617
powHexi <47748365+powHexi@users.noreply.github.com>
2021-11-20 19:54:02 +0200

    Finished

---
+++ Tests/SecondScenario.js
@@ -0,0 +1,11 @@
+//Перевірка покупки без авторизації
+const playwright = require('playwright');
+(async () => {
+     {
+      const browser = await playwright.chromium.launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto('https://www.demoblaze.com/prod.html?idp_=1');
+      await page.click('a[onclick^="addToCart"]');
+    }
+  })();
