b98d26a1f5ae2236f576c17872094f74e65ffda7
powHexi <47748365+powHexi@users.noreply.github.com>
2021-11-20 19:55:31 +0200

    Finished

--- Tests/SecondScenario.js
+++ Tests/SecondScenario.js
@@ -8,5 +8,6 @@ const playwright = require('playwright');
       await page.goto('https://www.demoblaze.com/prod.html?idp_=1');
       await page.click('a[onclick^="addToCart"]');
       console.log("Test failed.")
+      await browser.close();
     }
   })();
